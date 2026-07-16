6207643b0b6baa6529fb3dcd2c7d146d8b163fef
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,37 @@
 output "stack_hci_logical_networks_id" {
   description = "Map of id values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.id }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "stack_hci_logical_networks_custom_location_id" {
   description = "Map of custom_location_id values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.custom_location_id }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.custom_location_id if v.custom_location_id != null && length(v.custom_location_id) > 0 }
 }
 output "stack_hci_logical_networks_dns_servers" {
   description = "Map of dns_servers values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.dns_servers }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.dns_servers if v.dns_servers != null && length(v.dns_servers) > 0 }
 }
 output "stack_hci_logical_networks_location" {
   description = "Map of location values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.location }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "stack_hci_logical_networks_name" {
   description = "Map of name values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.name }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "stack_hci_logical_networks_resource_group_name" {
   description = "Map of resource_group_name values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.resource_group_name }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "stack_hci_logical_networks_subnet" {
   description = "Map of subnet values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.subnet }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.subnet if v.subnet != null && length(v.subnet) > 0 }
 }
 output "stack_hci_logical_networks_tags" {
   description = "Map of tags values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.tags }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 output "stack_hci_logical_networks_virtual_switch_name" {
   description = "Map of virtual_switch_name values across all stack_hci_logical_networks, keyed the same as var.stack_hci_logical_networks"
-  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.virtual_switch_name }
+  value       = { for k, v in azurerm_stack_hci_logical_network.stack_hci_logical_networks : k => v.virtual_switch_name if v.virtual_switch_name != null && length(v.virtual_switch_name) > 0 }
 }
 
